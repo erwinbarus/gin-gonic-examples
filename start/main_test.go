a23be5a7f6041d08e5d90d406bed52db30c761fd
Erwin Jelly Barus Lumbantobing <erwin.barus@xtremax.com>
2026-03-13 17:08:22 +0700

    add quick start

---
+++ start/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"message":"pong"}`, w.Body.String())
+}
